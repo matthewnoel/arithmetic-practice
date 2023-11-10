759f02581513129a5a6dbc228dbaeffe6029a85e
Matthew Noel <31493274+matthewnoel@users.noreply.github.com>
2023-11-10 08:21:40 -0800

    Added subtraction option

---
+++ src/components/QuestionModal.svelte
@@ -0,0 +1,69 @@
+<script>
+    export let selection;
+
+    const getRandomInt = (inclusiveMin, inclusiveMax) => {
+        inclusiveMin = Math.ceil(inclusiveMin);
+        inclusiveMax = Math.floor(inclusiveMax);
+        return Math.floor(
+            Math.random() * (inclusiveMax - inclusiveMin + 1) + inclusiveMin
+        );
+    };
+
+    const getProblem = () => {
+        switch (selection) {
+            case 1:
+                return generateSingleDigitAdditionProblem();
+            case 2:
+                return generateSingleDigitSubtractionProblem();
+            default:
+                break;
+        }
+        console.log("unknown selection");
+        alert("unkown selection");
+        return generateSingleDigitAdditionProblem();
+    };
+
+    const generateSingleDigitAdditionProblem = () => {
+        const term1 = getRandomInt(1, 9);
+        const term2 = getRandomInt(1, 9);
+        answer = term1 + term2;
+        problem = `${term1} + ${term2} =`;
+    };
+
+    const generateSingleDigitSubtractionProblem = () => {
+        const term1 = getRandomInt(1, 9);
+        const term2 = getRandomInt(1, 9);
+        answer = term1 - term2;
+        problem = `${term1} - ${term2} =`;
+    };
+
+    const handleCorrectAnswer = () => {
+        getProblem();
+        feedback = null;
+        value = null;
+    };
+
+    const handleIncorrectAnswer = () => {
+        feedback = `${value} isn't correct. Try again 😊`;
+        value = null;
+    };
+
+    const handleFormSubmission = () =>
+        value === answer ? handleCorrectAnswer() : handleIncorrectAnswer();
+
+    let problem;
+    let answer;
+    let feedback;
+    let value;
+
+    $: if (!problem) getProblem();
+</script>
+
+<form on:submit|preventDefault={handleFormSubmission}>
+    <label for="answer">{problem}</label>
+    <input type="number" name="answer" id="answer" bind:value />
+    <input type="submit" value="Submit" />
+    {#if feedback}
+        <p>{feedback}</p>
+    {/if}
+</form>
